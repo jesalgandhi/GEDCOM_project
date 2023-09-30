984e8a20e43a9d69994fbe9ebd475d8c533bc8bf
jesalgandhi <jesalgandhi9988@gmail.com>
2023-09-30 16:53:29 -0400

    finished US03, US14, added README

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to run:
+`script.py` is the main file to be executed along with the main gedcom file (`test_file.ged`)
+
+```
+python script.py test_file.ged
+```
+## Additional files
+Additional files like `sprint1.py` contain code for their respective sprint numbers. Additional gedcom files like `sprint1_test.ged` are used to test each sprint's code.
--- sprints/sprint1.py
+++ sprints/sprint1.py
@@ -1,22 +1,52 @@
 import sys
 import os
+from datetime import * 
 sys.path.append(os.path.join(sys.path[0], '..'))
 import script
 script.parse() # DO NOT DELETE
 
 # start code; families and individuals should be accessible now; test:
-print(script.families)
-print(script.individuals)
+# print(script.families)
+# print(script.individuals)
 
-for k in script.individuals:
-    print(script.individuals[k])
 
-# Jesal Gandhi Sprint1
-# USO3: Birth before death
-def US03():
-    pass
+def sprint1():
+    # Jesal Gandhi
+    # USO3: Birth before death: WORKING
+    def US03():
+        for k in script.individuals:
+            # print(script.individuals[k]['deathdate'])
+            deathdate_str = script.individuals[k]['deathdate']
+            birthdate_str = script.individuals[k]['birthdate']
+            if deathdate_str != 'NA':
+                death = datetime.strptime(deathdate_str, "%d %b %Y")
+                birth = datetime.strptime(birthdate_str, "%d %b %Y")
+                if birth > death:
+                    print(f"ERROR: INDIVIDUAL: US03: I{k}: Birthday {birthdate_str} occurs after death date {deathdate_str}")
+    # US14: Multiple births <= 5
+    def US14():
+        # print(script.individuals)
+        # print(script.families)
+        for k in script.families:
+            children = script.families[k]['CHIL']
+            if len(children) >= 5:
+                for i in range(len(children)):
+                    mutliple_births = 1
+                    same_date = ""
+                    for j in range(i+1, len(children)):
+                        same_date = script.individuals[children[i][1:]]['birthdate']
+                        if datetime.strptime(script.individuals[children[i][1:]]['birthdate'], "%d %b %Y") == datetime.strptime(script.individuals[children[j][1:]]['birthdate'], "%d %b %Y"):
+                            mutliple_births += 1
+                    if mutliple_births >= 5:
+                        print(f"ERROR: FAMILY: US14: {k}: There are 5 or more individuals born on the same date {same_date} in this family")
 
-# US14: Multiple births <= 5
-def US14():
-    pass
+    # ...
+
+    # call your US## here:
+    US03()
+    US14()
+
+sprint1()
+
+
 
